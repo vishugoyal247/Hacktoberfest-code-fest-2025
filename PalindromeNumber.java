47f438152deaf7c07313c73a63da5c494f0e9d9f
vishu247 <156403229+vishu247@users.noreply.github.com>
2025-10-15 22:58:20 +0530

    Create PalindromeNumber.java

---
+++ PalindromeNumber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        // Negative numbers and numbers ending with 0 (except 0 itself) are not palindrome
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversed = 0;
+        int original = x;
+
+        while (x > 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return original == reversed;
+    }
+}
